5d56020c8e29a54c3583297911e8a734e43c1683
StephanMalan <stephanmalan.rob@gmail.com>
2023-06-07 14:59:04 +0200

    Add support for multiple output formats

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
+use clap::ValueEnum;
 use serde::Deserialize;
+use serde::Serialize;
+use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use strfmt::strfmt;
 use toml;
 
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
     path: std::path::PathBuf,
+
+    /// Choose desired output format
+    #[arg(value_enum, default_value_t = PrintFormat::BASIC)]
+    format: PrintFormat,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum PrintFormat {
+    /// Prints output with the format: {name}:{version}
+    BASIC,
+    /// Prints output with the format: {icon} {name}:{version}
+    FORMAL,
+    /// Prints output with the format: {alt_icon} {name}:{version}
+    PLAYFUL,
+}
+
+#[derive(Serialize)]
+struct ProjectDetails {
+    icon: String,
+    alt_icon: String,
+    name: String,
+    version: String,
 }
 
 #[derive(Deserialize)]
@@ -45,13 +72,25 @@ fn main() {
     let full_path = fs::canonicalize(&args.path);
     if full_path.is_ok() {
         match get_project_name(full_path.unwrap()) {
-            Some(name) => println!("{}", name),
+            Some(details) => println!("{}", format_output(details, args.format)),
             None => (),
         }
     }
 }
 
-fn get_project_name(path: PathBuf) -> Option<String> {
+fn format_output(details: ProjectDetails, print_format: PrintFormat) -> String {
+    let format = match print_format {
+        PrintFormat::BASIC => "{name}:{version}",
+        PrintFormat::FORMAL => "{icon} {name}:{version}",
+        PrintFormat::PLAYFUL => "{alt_icon} {name}:{version}",
+    };
+    let serialized = serde_json::to_value(details).unwrap();
+    let hashmap: HashMap<String, String> = serde_json::from_value(serialized).unwrap();
+
+    return strfmt(format, &hashmap).unwrap();
+}
+
+fn get_project_name(path: PathBuf) -> Option<ProjectDetails> {
     let content = unwrap_or_return_none!(fs::read_dir(path.clone()));
     for entry in content {
         if let Ok(entry) = entry {
@@ -71,14 +110,24 @@ fn get_project_name(path: PathBuf) -> Option<String> {
     return get_project_name(path.parent().unwrap().to_path_buf());
 }
 
-fn read_cargo_toml(path: &PathBuf) -> Option<String> {
+fn read_cargo_toml(path: &PathBuf) -> Option<ProjectDetails> {
     let contents = unwrap_or_return_none!(fs::read_to_string(&path));
     let data: CargoData = unwrap_or_return_none!(toml::from_str(&contents));
-    return Some(data.package.name + &":".to_owned() + &data.package.version.to_owned());
+    return Some(ProjectDetails {
+        icon: "\u{e7a8}".to_owned(),
+        alt_icon: "🦀".to_owned(),
+        name: data.package.name,
+        version: data.package.version,
+    });
 }
 
-fn read_poetry_toml(path: &PathBuf) -> Option<String> {
+fn read_poetry_toml(path: &PathBuf) -> Option<ProjectDetails> {
     let contents = unwrap_or_return_none!(fs::read_to_string(&path));
     let data: PoetryToolData = unwrap_or_return_none!(toml::from_str(&contents));
-    return Some(data.tool.poetry.name + &":".to_owned() + &data.tool.poetry.version.to_owned());
+    return Some(ProjectDetails {
+        icon: "\u{e73c}".to_owned(),
+        alt_icon: "🐍".to_owned(),
+        name: data.tool.poetry.name,
+        version: data.tool.poetry.version,
+    });
 }
